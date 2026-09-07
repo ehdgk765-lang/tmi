b2ccd0f7cf46b47d9b9e2ac3f60207b44f302be6
김동하 <kdh@softin.co.kr>
2026-09-07 17:52:03 +0900

    config update

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,13 @@
 // firebase-config.js - TMI Firebase 초기화
 // ⚠️ 실제 Firebase 프로젝트 값으로 교체 필요
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAbJ0vi8OzwOq5wX1sd3HgeDWeJH3WdxiE",
+  authDomain: "tmi-tennis.firebaseapp.com",
+  projectId: "tmi-tennis",
+  storageBucket: "tmi-tennis.firebasestorage.app",
+  messagingSenderId: "733608731285",
+  appId: "1:733608731285:web:47f26a57e400882b2a719c",
+  measurementId: "G-85FL54485P"
 };
 
 firebase.initializeApp(firebaseConfig);
